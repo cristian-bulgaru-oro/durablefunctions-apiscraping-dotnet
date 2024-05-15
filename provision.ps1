dc451592d109487761d2c7c8e49665056934d813
Cristian Bulgaru <cristian.bulgaru@orange.com>
2024-05-15 13:36:31 +0300

    commit

--- provision.ps1
+++ provision.ps1
@@ -7,13 +7,13 @@ param (
 )
 
 # Creates a Resource group to hold our application
-az group create -l eastus -n $resourceGroup
+# az group create -l eastus -n $resourceGroup
 
 # Storage account where Azure Functions will keep its data and logs
-az storage account create --sku Standard_LRS --location EastUS --kind Storage -g $resourceGroup -n $storageName.ToLower()
+# az storage account create --sku Standard_LRS --location EastUS --kind Storage -g $resourceGroup -n $storageName.ToLower()
 
 # Creates our Function Application
-az functionapp create -n $functionName -g $resourceGroup -s $storageName.ToLower() -c eastus
+az functionapp create -n $functionName -g $resourceGroup -s $storageName.ToLower() -c northeurope
 
 # Configures Functions Application Settings
 az functionapp config appsettings set -g $resourceGroup -n $functionName --settings GitHubToken=$githubToken
